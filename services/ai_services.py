9797a3fd5b65e9e97c40fb0271e5aa918c56a5b8
vaibhav-catseyesystems <vaibhav.pachpute@catseyesystems.com>
2025-02-01 17:12:22 +0530

    new functions

--- services/ai_services.py
+++ services/ai_services.py
@@ -1,26 +1,26 @@
 import json
 import re
 from ai_model.ai_model import tokenizer,model
-from utils.ai_utils import create_event_details_prompt
+from utils.ai_utils import create_event_details_prompt,create_event_list_prompt, split_content_with_overlap
 import logging
+
+def model_response(prompt):
+    try:
+        text = tokenizer.apply_chat_template( prompt, tokenize=False, add_generation_prompt=True )
+        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+        generated_ids = model.generate(**model_inputs, max_new_tokens=2048)
+        generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)]
+        response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return response,None
+    except Exception as e:
+        return None,str(e)
 
 def get_event_details_with_ai(markdown,url):
     try:
-        messages=create_event_details_prompt(markdown=markdown,url=url)
-        text = tokenizer.apply_chat_template(
-            messages,
-            tokenize=False,
-            add_generation_prompt=True
-        )
-        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-        generated_ids = model.generate(
-            **model_inputs,
-            max_new_tokens=2048
-        )
-        generated_ids = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-        ]
-        response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        prompt=create_event_details_prompt(markdown=markdown,url=url)
+        response,error=model_response(prompt=prompt)
+        if error:
+            return {"data":None,"error":f"error while getting response from model {error}"}
         json_object_pattern = re.compile(r"\{.*?\}", re.DOTALL)
         match = json_object_pattern.findall(response)[0]
         try:
@@ -38,3 +38,31 @@ def get_event_details_with_ai(markdown,url):
     except Exception as e:
         logging.error(f"Exception while getting event details from AI model {e}")
         return {"data":None,"error":f"Exception while getting event details from AI model {e}"}
+
+def get_events_list_with_ai(markdown):
+    try:
+        chunks=split_content_with_overlap(markdown)
+        qwenResults=[]
+        for i, chunk in enumerate(chunks, start=1):
+            prompt=create_event_list_prompt(markdown=chunk)
+            response,error=model_response(prompt=prompt)
+            if error:
+                return {"data":None,"error":f"error while getting response from model {error}"}
+            qwenResults.append(str(response))
+        parsed_objects = []
+        for obj in qwenResults:
+            json_object_pattern = re.compile(r"\{.*?\}", re.DOTALL)
+            matches = json_object_pattern.findall(obj)
+            for match in matches:
+                try:
+                    event = json.loads(match)
+                    event_link = event["eventLink"]
+                    if not event_link.startswith("http"):
+                        print(f"{event_link} needs to reformatted...")
+                    parsed_objects.append(event)
+                except Exception as e:
+                    logging.error(f"Error processing event object {match} {e}")
+        return {"data":parsed_objects,"error":None}
+    except Exception as e:
+        logging.error(f"Exception while getting events list with ai {e}")
+        return {"data":None,"error":f"Exception while getting event link with AI {e}"}
--- utils/ai_utils.py
+++ utils/ai_utils.py
@@ -1,3 +1,8 @@
+def split_content_with_overlap(dom_content, max_length=30000, overlap=500):
+    chunks = []
+    for i in range(0, len(dom_content), max_length - overlap):
+        chunks.append(dom_content[i : i + max_length])
+    return chunks
 
 def create_event_details_prompt(markdown,url):
     messages=[
@@ -20,3 +25,11 @@ def create_event_details_prompt(markdown,url):
     return messages
 
 
+def create_event_list_prompt(markdown):
+    prompt=f"""
+    Below i have given markdown content of html webpage
+    Extract all event/conference related links links from the given HTML, excluding non-event links like press, privacy policy, work-with-us, about-us, contact-us, etc. Do not return code snippet,other explanation only return links.
+    give response in json format like [{{eventName:eventname,eventDate:event date,eventLink:eventLink}}] and nothing else
+    {markdown}
+    """
+    return prompt
